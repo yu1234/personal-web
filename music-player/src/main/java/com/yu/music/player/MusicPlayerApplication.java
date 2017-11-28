25420545e28700cc786c800bf8b5f6bb6b6b9473
yu <836986488@qq.com>
2017-11-28 16:34:08 +0800

    init

---
+++ music-player/src/main/java/com/yu/music/player/MusicPlayerApplication.java
@@ -0,0 +1,12 @@
+package com.yu.music.player;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicPlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicPlayerApplication.class, args);
+	}
+}
